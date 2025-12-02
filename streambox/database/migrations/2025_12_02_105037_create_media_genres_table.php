bddc55f21d647b555e2b304240bc140a00dba993
Arkaitz <ikdge@plaiaundi.net>
2025-12-02 12:01:22 +0100

    Migrations

---
+++ streambox/database/migrations/2025_12_02_105037_create_media_genres_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media_genres', function (Blueprint $table) {
+
+            //La tablas pivote no tienen id, solo defienen las relaciones
+            $table->foreignId('media_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+            
+            $table->primary(['media_id', 'genre_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media_genres');
+    }
+};
